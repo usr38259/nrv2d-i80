f177de333e3fc3accf942a887e539e01a59f775a
usr38259 <88048930+usr38259@users.noreply.github.com>
2022-04-08 18:48:46 +0300

    Add files via upload

--- n2dtest/n2dtest.c
+++ n2dtest/n2dtest.c
@@ -6,7 +6,6 @@
 
 #define TEXT_BIN	"packed.dat"
 #define UNPKDATA_FILE "unpacked.dat"
-#define UNPKDATA_OFFSET 0x1EC1
 
 #define PFREQ 2000000
 
@@ -21,22 +20,22 @@ char unpkdata [65536];
 int _CRTAPI1 main ()
 {
 	FILE *f;
-	long fl, ts = 0;
+	long fl, pkl, ts = 0;
 
 	memset (mem, 0, sizeof (mem));
 	f = fopen (TEXT_BIN, "rb");
 	if (f == NULL) {
-		puts ("BIN file '" TEXT_BIN "' open error");
+		perror ("BIN file '" TEXT_BIN "' open error");
 		return 1;
 	}
-	fl = filelength (fileno (f));
-	if (fl <= 0 || fl >= 65536) {
+	pkl = filelength (fileno (f));
+	if (pkl <= 0 || pkl >= 65536) {
 		puts ("File length error");
 		fclose (f);
 		return 1;
 	}
-	if (fread (mem, fl, 1, f) != 1) {
-		puts ("BIN file read error");
+	if (fread (mem, pkl, 1, f) != 1) {
+		perror ("BIN file '" TEXT_BIN "' read error");
 		fclose (f);
 		return 1;
 	}
@@ -44,9 +43,9 @@ int _CRTAPI1 main ()
 
 	i8080_cpu_init (&i80);
 	i808x_cpu_reset (&i80);
-	i80.SP.w.l = 0x7ffc;
-	*(unsigned short*)&mem [0x7ffe] = 0x7676;
-	*(unsigned short*)&mem [0x7ffc] = 0x7ffe;
+	i80.SP.w.l = 0xfffc;
+	*(unsigned short*)&mem [0xfffe] = 0x7676;
+	*(unsigned short*)&mem [0xfffc] = 0xfffe;
 	i80.PC.w.l = 0x0000;
 
 	while (1) {
@@ -54,38 +53,40 @@ int _CRTAPI1 main ()
 			printf ("Halt at %04X\n", i80.PC.w.l);
 			break;
 		}
-		if (ts < 0) {
-			puts ("Time exceed");
-			break;
-		}
+		if (ts < 0) {	puts ("Time exceed"); break; }
 		i80.icount = 0;
 		i808x_cpu_execute (&i80);
 		ts -= i80.icount;
 	}
-	f = fopen (UNPKDATA_FILE, "rb");
+	if (ts > 0) {
+		printf ("%9d tcks; %.6f s (%f MHz)\n", ts, (float)ts / PFREQ, (float)PFREQ * 1e-6);
+		f = fopen (UNPKDATA_FILE, "rb");
+	}	else	return 1;
 	if (f != NULL) {
 		fl = filelength (fileno (f));
 		if (fl <= 0 || fl >= 65536) {
 			puts ("File '" UNPKDATA_FILE "' length error");
 			fclose (f);
-			goto ts;
+			return 1;
 		}
 		if (fread (unpkdata, fl, 1, f) != 1) {
-			puts ("File '" UNPKDATA_FILE "' read  error:");
-			perror (NULL);
+			perror ("File '" UNPKDATA_FILE "' read error");
 			fclose (f);
-			goto ts;
+			return 1;
 		}
-		if (memcmp (mem + UNPKDATA_OFFSET, unpkdata, fl) == 0) {
-			printf ("Unpacked data file '" UNPKDATA_FILE
-				"' and memory at offset %04X are identical\n", UNPKDATA_OFFSET);
-		} else	puts ("Unpacked data file '" UNPKDATA_FILE "' and memory ARE NOT IDENTICAL");
 		fclose (f);
+		if (pkl + fl <= 65536) {
+			if (memcmp (mem + pkl, unpkdata, fl) == 0)
+				printf ("Unpacked data file '" UNPKDATA_FILE
+					"' and memory at offset %04X are identical\n", (int)pkl);
+			else	puts ("Unpacked data file '" UNPKDATA_FILE "' and memory ARE NOT IDENTICAL");
+		} else {
+			puts ("Size of unpacked data is out of memory");
+			return 1;
+		}
 	} else {
-		puts ("Unpacked data file '" UNPKDATA_FILE "' open error:");
-		perror (NULL);
+		perror ("Unpacked data file '" UNPKDATA_FILE "' open error");
+		return 1;
 	}
-ts:	if (ts > 0)
-		printf ("%9d tcks; %.6f s\n", ts, (float)ts / PFREQ);
 	return 0;
 }
